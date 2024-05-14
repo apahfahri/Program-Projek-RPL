66c2036f54aefa7bf2e505e4a35bd88a1b27bd0b
apahfahri <ahmadfahrie19@gmail.com>
2024-05-14 17:18:01 +0700

    add carousel

--- index.php
+++ index.php
@@ -17,19 +17,18 @@ include('layout/header.php')
 </head>
 
 <body>
-
-    <div class="container-fluid pb-3">
+    <div class="container-fluid pb-3 mb-3">
             <div class="bg-body-tertiary border rounded-3">
                 <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active" data-bs-interval="10000">
-                            <img src="asset/game/Valorant.jpeg" class="d-block w-100" alt="...">
+                            <img src="image/crsl01.jpg" class="d-block w-100" alt="...">
                         </div>
                         <div class="carousel-item" data-bs-interval="2000">
-                            <img src="asset/game/Counter Strike 2.jpeg" class="d-block w-100" alt="...">
+                            <img src="image/crsl02.jpg" class="d-block w-100" alt="...">
                         </div>
                         <div class="carousel-item">
-                            <img src="asset/game/League Of Legends.jpeg" class="d-block w-100" alt="...">
+                            <img src="image/crsl03.jpg" class="d-block w-100" alt="...">
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
