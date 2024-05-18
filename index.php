a3bc6c5164ca32699ef2e4e2688f3435725213df
apahfahri <ahmadfahrie19@gmail.com>
2024-05-18 19:04:43 +0700

    update background sign page

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ include('layout/header.php')
 <body>
     <div class="container-fluid pb-3 mb-3">
             <div class="bg-body-tertiary border rounded-3">
-                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                <div id="carouselGamePage" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active" data-bs-interval="10000">
                             <img src="image/crsl01.jpg" class="d-block w-100" alt="...">
@@ -31,11 +31,11 @@ include('layout/header.php')
                             <img src="image/crsl03.jpg" class="d-block w-100" alt="...">
                         </div>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselGamePage" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselGamePage" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
