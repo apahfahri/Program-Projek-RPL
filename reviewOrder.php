4662e30ac10ac2c7a8649272c94af5468da60299
2Phirkuadrat <lutfi3522@gmail.com>
2024-05-30 10:05:24 +0700

    Fix reviewOrder

--- reviewOrder.php
+++ reviewOrder.php
@@ -13,22 +13,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $stmt->bind_param('sii', $review, $rating, $order_id);
 
     if ($stmt->execute()) {
-        $query = "
-            UPDATE workers w
-            JOIN (
-                SELECT Id_Worker, AVG(Rating) AS avg_rating
-                FROM `order`
-                GROUP BY Id_Worker
-            ) o ON w.Id_Worker = o.Id_worker
-            SET w.rating = o.avg_rating;
-        ";
-
-        if ($conn->query($query) === TRUE) {
-            echo "Ratings updated successfully.";
-        } else {
-            echo "Error updating ratings: " . $conn->error;
-        }
-
         header('Location: myOrder.php?success=Successfully provided a review');
         exit();
     } else {
